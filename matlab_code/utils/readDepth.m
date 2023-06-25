b74c6220d2892438e0824dd303aaf1b1b5bba457
Hongxiao Yu <112234465+hongxiaoy@users.noreply.github.com>
2023-06-25 15:14:17 +0800

    Update readDepth.m

--- matlab_code/utils/readDepth.m
+++ matlab_code/utils/readDepth.m
@@ -1,5 +1,6 @@
 function depthInpaint = readDepth(depthpath)         
     depthVis = imread(depthpath);  % 读取一个深度图像
+    % 利用 bitor 或运算、bitshift 位移运算, -n 为右移, n 为左移
     depthInpaint = bitor(bitshift(depthVis,-3), bitshift(depthVis,16-3));  % 0123 4567 89AB CDEF --> DEF0 1234 5678 9ABC
     depthInpaint = double(depthInpaint)/1000; 
 end
